0a6154dd821f0e60f95363330eb86b0cda3d4433
mpebiser <mpebiser@gmail.com>
2024-07-04 18:29:53 +0300

    Esp 32 frontend for displaying temperature

---
+++ ESP32_WebSocket_Server.ino
@@ -0,0 +1,156 @@
+/*********
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete instructions at https://RandomNerdTutorials.com/esp32-websocket-server-sensor/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "LittleFS.h"
+#include <Arduino_JSON.h>
+
+
+// Replace with your network credentials
+const char* ssid = "ESP_SOCKET";
+const char* password = "00001111";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Create a WebSocket object
+AsyncWebSocket ws("/ws");
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
+
+
+//variable
+float roll, pitch, yaw, temp = 0.0;
+
+
+// Timer variables
+unsigned long lastTime = 0;
+unsigned long timerDelay = 30000;
+
+
+//******************** Get Sensor Readings and return JSON object  *********************
+//**************************************************************************************
+String getSensorReadings(){
+  readings["roll"]          = String(roll);
+  readings["pitch"]         = String(pitch);
+  readings["yaw"]           = String(yaw);
+  readings["temperature"]   = String(temp);
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+//****************************** Initialize LittleFS **********************************
+//*************************************************************************************
+void initLittleFS() {
+  if (!LittleFS.begin(true)) {
+    Serial.println("An error has occurred while mounting LittleFS");
+  }
+  Serial.println("LittleFS mounted successfully");
+}
+
+//*************************   Initialize WiFi ****************************************
+//************************************************************************************
+void initWiFi(int a) {
+  if (a == 0){
+    // Setting the ESP as an access point
+    Serial.print("Setting AP (Access Point)…");
+    // Remove the password parameter, if you want the AP (Access Point) to be open
+    WiFi.softAP(ssid, password);
+    Serial.println();
+    
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);  
+    delay(1000);
+      
+  }
+  else if(a == 1){
+    // Connect to existing WIFI
+    WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid, password);
+      Serial.print("Connecting to WiFi ..");
+      while (WiFi.status() != WL_CONNECTED) {
+        Serial.print('.');
+        delay(1000);
+      }
+      Serial.println(WiFi.localIP());
+  }
+}
+
+void notifyClients(String sensorReadings) {
+  ws.textAll(sensorReadings);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    //data[len] = 0;
+    //String message = (char*)data;
+    // Check if the message is "getReadings"
+    //if (strcmp((char*)data, "getReadings") == 0) {
+      //if it is, send current sensor readings
+      String sensorReadings = getSensorReadings();
+      Serial.print(sensorReadings);
+      notifyClients(sensorReadings);
+    //}
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+void setup() {
+  Serial.begin(115200);
+  initWiFi(0);
+  initLittleFS();
+  initWebSocket();
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+
+    
+//    String sensorReadings = getSensorReadings();
+    String sensorReadings = getSensorReadings();
+    Serial.println(sensorReadings);
+    notifyClients(sensorReadings);
+    lastTime = millis();
+  }
+  ws.cleanupClients();
+}
